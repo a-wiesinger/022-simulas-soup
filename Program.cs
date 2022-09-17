b6c70428bbddf9122e1192532a5f3a56cb1b9f11
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-09-17 06:52:56 -0400

    Create enums

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Simula's Soup
+:: Tuples ::
+*/
+
+// Criteria:
+// DONE - Define enums for Food:
+    // Type (Soup, Stew, Gumbo)
+    // Main Ingredient (Mushrooms, Chicken, Carrots)
+    // Seasoning (Spicy, Salty, Sweet)
+// Create a tuple which represents a soup composed of the 3 enum types we defined
+// User selects the type, main ingredient, and seasoning
+// Display the contents of the soup tuple variable e.g. "Sweet Chicken Gumbo"
+
+Console.Title = "Simula's Soup";
+Console.Clear();
+
+// Create tuple for souple ;)
